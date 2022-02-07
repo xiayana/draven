fea597889e06f1615866a041a4feba52260bc7f8
xy5211314 <xiayan@8lab.cn>
2022-02-07 15:31:12 +0800

    数据库链接

--- draven-dsl-engine/src/main/java/com/lab8/engine/listener/KafkaConsumer.java
+++ draven-dsl-engine/src/main/java/com/lab8/engine/listener/KafkaConsumer.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.Map;
 
 @Slf4j
@@ -20,7 +19,7 @@ public class KafkaConsumer {
     //下面的主题是一个数组，可以同时订阅多主题，只需按数组格式即可，也就是用“，”隔开
     @KafkaListener(containerFactory = "kafkaListenerContainerFactory", topics = {"${kafka.consumer.topic}"})
     public void receive(String records) {
-        log.info("消费得到的消息---value: " + records);
+       // log.info("消费得到的消息---: " + records);
         try {
             EPRuntime epRuntime = epServiceProvider.getEPRuntime();
             Map<String,String> json = JSONObject.parseObject(records, Map.class);
